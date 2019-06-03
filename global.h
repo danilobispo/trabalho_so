5e0683ba3e1bf3f9b148ad8a423fafc5eb33ed6d
HelenaSILS <40037397+HelenaSILS@users.noreply.github.com>
2019-06-03 18:52:48 +0100

    nomes

--- global.h
+++ global.h
@@ -1,3 +1,11 @@
+/*
+* Grupo:
+* Laís Mendes Gonçalves (110033647)
+* Guilherme de Oliveira Silva (110012496)
+* Danilo José Bispo Galvão (120114852)
+* Helena Schubert I. L. Silva (100012311)
+*/
+
 #ifndef _GLOBAL_H
 #define _GLOBAL_H
 
